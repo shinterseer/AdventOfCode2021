8b600df0f5066c8555ddb927f0d00fa70155c4cf
SImon Hinterseer <hinterseer.simon@gmail.com>
2021-12-07 19:35:18 +0100

    day07 convergence_scheme

--- 07_script.py
+++ 07_script.py
@@ -1,6 +1,6 @@
 import numpy as np
 import time
-
+import matplotlib.pyplot as plt
 
 def get_lines(filename):
     file = open(filename)
@@ -11,12 +11,12 @@ def get_lines(filename):
 def sum_to_n(n):
     return n*(n+1) / 2
 
-
 def fuel_cost1(positions, point):
     cost = 0
     for pos in positions:
-        cost += abs(pos - optimal1)
+        cost += abs(pos - point)
     return cost
+
 
 def fuel_cost2(positions, point):
     cost = 0
@@ -24,6 +24,28 @@ def fuel_cost2(positions, point):
         dist = abs(point - pos)
         cost += sum_to_n(dist)
     return cost
+
+
+def convergence_step(positions,mmin,mmax):
+    mid = .5*(mmax + mmin)
+    up = fuel_cost2(positions,mid + 1)
+    down = fuel_cost2(positions,mid - 1)
+    if up > down:
+        return mmin, mid
+    else:
+        return mid, mmax
+
+
+def convergence_scheme(positions,mmin,mmax):
+    steps = 0
+    while True:
+        mmin_old = mmin
+        mmax_old = mmax
+        mmin, mmax = convergence_step(positions,mmin,mmax)
+        steps += 1
+        if (abs(mmin - mmin_old) < 1) and (abs(mmax - mmax_old) < 1): break
+        if steps > 1000: break
+    return mmin, mmax, steps
 
 
 if __name__ == '__main__':
@@ -45,4 +67,9 @@ if __name__ == '__main__':
     print("Answer 2: ", fuel_cost2(positions,optimal2))
     
     
+    # but if you like numerics... you write an iteration scheme...
+    bottom, top, steps = convergence_scheme(positions, -1000, 2000)
+    optimal2 = round((bottom+top)*.5)
+    print("Answer 2 (cool version): ", fuel_cost2(positions,optimal2))
+    print("concergence after ", steps, "steps")
     
